b9bb7fe86c22435304772a0256a34f20d239463b
Tamilkidszona <89377934+Tamilkidszona@users.noreply.github.com>
2021-08-23 14:20:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+class Config(Config):
+
+    TG_BOT_TOKEN= "1961753507:AAFxQLkxq33VzJDz9xb16_hV7qQ19qnRtSA" #imp
+
+    APP_ID = 7073816 #imp
+
+    API_HASH = "7cb448a441865808cd8add4201f24aba" #imp
+
+    AUTH_CHANNEL = [-1001522510478, 1131894770] #imp replace your_id with your id from telegram or delete
+
+    INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev
+    
+    GLEECH_COMMAND = "gleech@Torr_Kidszona_bot"
+    
+    YTDL_COMMAND = 'ytdl@Torr_Kidszona_bot'
+
+    TELEGRAM_LEECH_COMMAND_G = "tleech@Torr_Kidszona_bot"
+
+    CLONE_COMMAND_G = "gclone@kidszonaleech"
+
+    PYTDL_COMMAND_G = "pytdl@kidszonaleech"
+
+    DESTINATION_FOLDER = "TorrentLeech-Gdrive"
+
+    LEECH_COMMAND = "leech@Torr_Kidszona_bot"
+
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.-XX9fgU--","token_type":"Bearer","refresh_token":"1//0gLZEp8-VV2rZyourtCgYIARAAGBASNwF-","expiry":"2020-09-03T14:22:34.599776393Z"}\nteam_drive = """
+
+    #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
